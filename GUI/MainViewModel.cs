2c8f281db48afe0d08a9c11ca383891337d77ece
Jonas Hvid Jensen <jona67f6@edu.campusvejle.dk>
2020-10-01 14:44:18 +0200

    Fetch teams and show them in GUI on startup

---
+++ GUI/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Model;
+using System.Collections.ObjectModel;
+
+namespace GUI {
+    class MainViewModel {
+        public ObservableCollection<Team> Teams { get; private set; }
+
+        public MainViewModel(IDatabase database) {
+            Teams = new ObservableCollection<Team>();
+            foreach (var team in database.AllTeams()) {
+                Teams.Add(team);
+            }
+        }
+    }
+}
