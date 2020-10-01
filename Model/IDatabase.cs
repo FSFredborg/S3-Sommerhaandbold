09a97b352752a538fe2bb77951bf54cf790bdaf6
Jonas Hvid Jensen <jona67f6@edu.campusvejle.dk>
2020-10-01 14:22:52 +0200

    Add documentation for database interface
    Move to its own package, Model, since it isn't directly related to the
    GUI.

---
+++ Model/IDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Model {
+    public interface IDatabase {
+        List<Team> AllTeams();
+
+        // Delete the given team if it exists. Only the Id property is used.
+        void DeleteTeam(Team team);
+
+        // Add the given team to the database. The Id property is updated to
+        // match the database.
+        void AddTeam(Team team);
+
+        // Update the matching database record (by Id), such that any changes
+        // to properties are changed.
+        void UpdateTeam(Team team);
+    }
+
+    // Team is a plain object. You may change its properties; to save such
+    // changes, use IDatabase.UpdateTeam.
+    public struct Team {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
