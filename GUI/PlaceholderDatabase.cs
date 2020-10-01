9e9afa952474f37c634b53711815e2a81be8c7e0
Jonas Hvid Jensen <jona67f6@edu.campusvejle.dk>
2020-10-01 14:31:33 +0200

    Add placeholder database to GUI package
    We don't have actual database support yet, so we can use this class for
    testing.

---
+++ GUI/PlaceholderDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Model;
+
+namespace GUI {
+    class PlaceholderDatabase : IDatabase {
+        void IDatabase.AddTeam(Team team) {
+            System.Windows.MessageBox.Show($"Added team '{team.Name}'.");
+        }
+
+        List<Team> IDatabase.AllTeams() {
+            return new List<Team> {
+                new Team { Id = 0, Name = "Team 0" },
+                new Team { Id = 1, Name = "Team 1" },
+                new Team { Id = 2, Name = "Team 2" },
+            };
+        }
+
+        void IDatabase.DeleteTeam(Team team) {
+            System.Windows.MessageBox.Show($"Deleted team with ID '{team.Id}'.");
+        }
+
+        public void UpdateTeam(Team team) {
+            System.Windows.MessageBox.Show($"Updated team with ID '{team.Id}'.");
+        }
+    }
+}
