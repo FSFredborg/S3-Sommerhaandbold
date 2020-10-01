59955e0264bebfba59ac9c90c10c516265c847df
Jonas Hvid Jensen <jona67f6@edu.campusvejle.dk>
2020-10-01 13:51:12 +0200

    Add interface for database layer

---
+++ IDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S3_Sommerhaandbold {
+    interface IDatabase {
+        List<Team> AllTeams();
+        void DeleteTeam(Team team);
+        void AddTeam(Team team);
+    }
+
+    class Team {
+        int Id { get; set; }
+        string Name { get; set; }
+    }
+}
