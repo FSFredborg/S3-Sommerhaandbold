3f75e96878a307dfa6b067a8c2a21ab3c5a0cd69
Jonas Hvid Jensen <jona67f6@edu.campusvejle.dk>
2020-10-27 11:28:47 +0100

    Sketch out GUI for teams and layers

---
+++ GUI/LayerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GUI {
+    class LayerViewModel {
+        private readonly int layer;
+
+        public LayerViewModel(int layer) {
+            this.layer = layer;
+        }
+
+        public string Title { get => $"Layer {layer}"; }
+    }
+}
--- GUI/MainViewModel.cs
+++ GUI/MainViewModel.cs
@@ -5,7 +5,12 @@ namespace GUI {
     class MainViewModel {
         public ObservableCollection<Team> Teams { get; private set; }
 
+        public ObservableCollection<LayerViewModel> Layers { get; private set; }
+
         public MainViewModel(IDatabase database) {
+            Layers = new ObservableCollection<LayerViewModel>();
+            for (int i = 1; i <= 4; ++i) Layers.Add(new LayerViewModel(i));
+
             Teams = new ObservableCollection<Team>();
             foreach (var team in database.AllTeams()) {
                 Teams.Add(team);
